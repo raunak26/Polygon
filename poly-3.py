1e4e6497e30741580c62ed233c633f97de967b8b
Raunak Anand <46580556+raunak26@users.noreply.github.com>
2019-03-07 18:24:54 -0800

    Add files via upload

---
+++ poly-3.py
@@ -0,0 +1,32 @@
+# poly.py
+# Raunak Anand
+# ECS 36A Winter Quarter
+# Write a program that reads an integer n
+# and draws a regular polygon with n sides
+
+import turtle               # allows us to use turtles
+
+# size of the rectangle's sides
+side = 50
+
+# set up the drawing
+# window stuff first
+
+window = turtle.Screen()        # draw the window for the picture
+poly = turtle.Turtle()          # create a turtle, called "poly"
+try:
+    n = int(input("How many sides would you like your polygon to have?: "))
+    poly.shape("turtle")
+    poly.speed(3)                # a turtle is slow (10 fastest, 1 slowest)
+    poly.color("red")          # make colour of the pen white
+    poly.pensize(2)              # make the lines 2 pixels
+
+    # draw the box
+    for i in range(int(n)):
+        poly.forward(side)             # tell box to move forward some
+        poly.right(360/int(n))               # tell box to turn right
+
+except:
+    print("Please print an integer")
+
+window.mainloop()               # wait until the user closes the window
